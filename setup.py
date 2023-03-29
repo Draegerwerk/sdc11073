193ebce0f433712df9616490746825c1338140c7
Budnick, Leon <leon.budnick@draeger.com>
2023-03-29 10:10:02 +0200

    fixe

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 import os
 import subprocess
+# To use a consistent encoding
+from codecs import open
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages
+from setuptools import setup
+
 
 def get_build_version():
     """
@@ -32,7 +35,8 @@ try:
     gitrev = gitrev.decode('ascii')
 except:
     import traceback
-    print (traceback.format_exc())
+
+    print(traceback.format_exc())
     gitrev = '-'
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -44,9 +48,6 @@ with open(os.path.join(here, 'sdc11073/version.py'), 'w') as v:
 # Get the long description from the README file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-
-dependencies = ['lxml>=2.3',
-                'ifaddr']
 
 setup(
     name='sdc11073',
@@ -99,15 +100,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=dependencies,
+    install_requires=['lxml>=2.3',
+                      'ifaddr'],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-#    extras_require={
-#        'dev': ['check-manifest'],
-#        'test': ['coverage'],
-#    },
+    extras_require={
+        'lz4': ['lz4'],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -126,14 +127,14 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-#    data_files=[('my_data', ['data/data_file'])],
+    #    data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-#    entry_points={
-#        'console_scripts': [
-#            'sample=sample:main',
-#        ],
-#    },
+    #    entry_points={
+    #        'console_scripts': [
+    #            'sample=sample:main',
+    #        ],
+    #    },
 )
