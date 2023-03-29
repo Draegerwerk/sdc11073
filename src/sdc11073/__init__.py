dfd8121b00387556cce74c1e8a2ab1308fb9c8b4
Budnick, Leon <leon.budnick@draeger.com>
2023-03-29 16:12:39 +0200

    fix tst run

--- src/sdc11073/__init__.py
+++ src/sdc11073/__init__.py
@@ -1,1 +1,15 @@
+from . import commlog
+from . import compression
+from . import location
+from . import mdib
+from . import namespaces
+from . import netconn
+from . import pmtypes
+from . import pysoap
+from . import safety
+from . import sdcclient
+from . import sdcdevice
+from . import wsdiscovery
+from . import xmlparsing
+
 __version__ = '0.0.0'
