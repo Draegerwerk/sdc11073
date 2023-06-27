abef20964be6df42c2096fe3b0b9539662925f7d
Budnick, Leon <leon.budnick@draeger.com>
2023-06-27 14:45:58 +0200

    automatic linting

--- src/sdc11073/sdcclient/sdcclientimpl.py
+++ src/sdc11073/sdcclient/sdcclientimpl.py
@@ -1,38 +1,37 @@
-""" Using lxml based SoapClient"""
+"""Using lxml based SoapClient."""
 from __future__ import annotations
 
 import copy
-import functools
 import ssl
 import traceback
 import uuid
 from dataclasses import dataclass
-from typing import Optional, List, Union, Set, Any, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 from urllib.parse import urlparse, urlsplit
 
 from lxml import etree as etree_
 
+from sdc11073 import commlog, loghelper, network
+from sdc11073 import observableproperties as properties
+from sdc11073.definitions_base import ProtocolsRegistry
+from sdc11073.dispatch import DispatchKey, MessageConverterMiddleware
+from sdc11073.exceptions import ApiUsageError
+from sdc11073.httpserver import compression
+from sdc11073.httpserver.httpserverimpl import HttpServerThreadBase
+from sdc11073.namespaces import EventingActions
+from sdc11073.xml_types import eventing_types, mex_types
+from sdc11073.xml_types.addressing_types import HeaderInformationBlock
+from sdc11073.xml_types.dpws_types import DeviceEventingFilterDialectURI
+from sdc11073.xml_types.wsd_types import ProbeMatchesType, ProbeType
+
 from .components import default_sdc_client_components
 from .request_handler_deferred import EmptyResponse
-from .subscription import ClSubscription
-from .. import commlog, network
-from .. import loghelper
-from .. import observableproperties as properties
-from ..definitions_base import ProtocolsRegistry
-from ..dispatch import DispatchKey, MessageConverterMiddleware
-from ..dispatch.request import RequestData
-from ..exceptions import ApiUsageError
-from ..httpserver import compression
-from ..httpserver.httpserverimpl import HttpServerThreadBase
-from ..namespaces import EventingActions
-from ..xml_types import eventing_types
-from ..xml_types import mex_types
-from ..xml_types.addressing_types import HeaderInformationBlock
-from ..xml_types.dpws_types import DeviceEventingFilterDialectURI
-from ..xml_types.wsd_types import ProbeType, ProbeMatchesType
 
 if TYPE_CHECKING:
-    from ..xml_types.mex_types import HostedServiceType
+    from sdc11073.dispatch.request import RequestData
+    from sdc11073.xml_types.mex_types import HostedServiceType
+
+    from .subscription import ClSubscription
 
 
 class HostDescription:
@@ -42,7 +41,7 @@ class HostDescription:
         self.this_device = dpws_envelope.this_device
         self.host = dpws_envelope.host
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'HostDescription: this_model = {self.this_model}, this_device = {self.this_device}, host = {self.host}'
 
 
@@ -90,33 +89,8 @@ class HostedServiceDescription:
             self._logger.error(
                 f'could not read wsdl from {actual_path}: error={ex}, data=\n{self.wsdl_bytes}')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.__class__.__name__} "{self.service_id}" endpoint = {self._endpoint_address}'
-
-
-def ip_addr_to_int(ip_string):
-    """ Convert string like '192.168.0.1' to an integer
-    (helper for sort_ip_addresses)"""
-    return functools.reduce(lambda x, y: x * 256 + y, (int(x) for x in ip_string.split('.')), 0)
-
-
-def _cmp(address_a, address_bb, _ref_int):
-    """ helper for sort_ip_addresses"""
-    _a_abs = abs(ip_addr_to_int(address_a) - _ref_int)
-    _b_abs = abs(ip_addr_to_int(address_bb) - _ref_int)
-    diff = _a_abs - _b_abs
-    if diff < 0:
-        return -1
-    if diff > 0:
-        return 1
-    return 0
-
-
-def sort_ip_addresses(addresses, ref_ip):
-    """ sorts list addresses by distance to refIP, shortest distance first"""
-    _ref = ip_addr_to_int(ref_ip)
-    addresses.sort(key=lambda a: abs(ip_addr_to_int(a) - _ref))
-    return addresses
 
 
 @dataclass(frozen=True)
@@ -157,11 +131,12 @@ class _NotificationsSplitter:
 
 
 class SdcClient:
-    """ The SdcClient can be used with a known device location.
+    """The SdcClient can be used with a known device location.
     The location is typically the result of a wsdiscovery process.
     This class expects that the BICEPS services are available in the device.
-    What if not???? => raise exception in _discover_hosted_services
+    What if not???? => raise exception in _discover_hosted_services.
     """
+
     is_connected = properties.ObservableProperty(False)  # a boolean
 
     # observable properties for all notifications
@@ -188,13 +163,12 @@ class SdcClient:
     SSL_CIPHERS = None  # None : use SSL default
 
     def __init__(self, device_location: str, sdc_definitions, ssl_context,
-                 epr: Union[str, uuid.UUID, None] = None,
+                 epr: str | uuid.UUID | None = None,
                  validate=True,
                  log_prefix='',
                  default_components=None, specific_components=None,
                  chunked_requests=False):  # pylint:disable=too-many-arguments
-        """
-        :param device_location: the XAddr location for meta data, e.g. http://10.52.219.67:62616/72c08f50-74cc-11e0-8092-027599143341
+        """:param device_location: the XAddr location for meta data, e.g. http://10.52.219.67:62616/72c08f50-74cc-11e0-8092-027599143341
         :param sdc_definitions: a class derived from BaseDefinitions
         :param epr: the path of this client in http server
         :param ssl_context: used for ssl connection to device and for own HTTP Server (notifications receiver)
@@ -220,7 +194,7 @@ class SdcClient:
         self._logger = loghelper.get_logger_adapter('sdc.client', self.log_prefix)
         self._network_adapter = self._get_host_adapter_by_device_location()
         self._logger.info('SdcClient for {} uses own IP Address {}', self._device_location, self._network_adapter)
-        self.host_description: Optional[mex_types.Metadata] = None
+        self.host_description: mex_types.Metadata | None = None
         self.hosted_services = {}  # lookup by service id
         self._validate = validate
         try:
@@ -269,7 +243,7 @@ class SdcClient:
             self.msg_reader, self._msg_factory, self._logger, self._services_dispatcher)
 
     def set_mdib(self, mdib):
-        """ SdcClient sometimes must know the mdib data (e.g. Set service, activate method)."""
+        """SdcClient sometimes must know the mdib data (e.g. Set service, activate method)."""
         if mdib is not None and self._mdib is not None:
             raise ApiUsageError('SdcClient has already an registered mdib')
         self._mdib = mdib
@@ -277,7 +251,6 @@ class SdcClient:
             self.client('Set').register_mdib(mdib)
         if self.client('Context') is not None:
             self.client('Context').register_mdib(mdib)
-        # self._msg_factory.register_mdib(mdib)
 
     @property
     def mdib(self):
@@ -322,13 +295,13 @@ class SdcClient:
 
     def mk_subscription(self, dpws_hosted: HostedServiceType,
                         filter_type: eventing_types.FilterType,
-                        actions: List[DispatchKey]) -> ClSubscription:
-        """ creates a subscription object and registers it in dispatcher
+                        actions: list[DispatchKey]) -> ClSubscription:
+        """Creates a subscription object and registers it in dispatcher
         :param dpws_hosted: proxy for the hosted service that provides the events we want to subscribe to
                            This is the target for all subscribe/unsubscribe ... messages
         :param filter_type: the filter that is sent to device
         :param actions: a list of DispatchKey that this subscription shall handle.
-        :return: a subscription object
+        :return: a subscription object.
         """
         subscription = self._subscription_mgr.mk_subscription(dpws_hosted, filter_type)
         # direct subscribed notifications to this subscription
@@ -338,11 +311,11 @@ class SdcClient:
 
     def do_subscribe(self, dpws_hosted: HostedServiceType,
                      filter_type: eventing_types.FilterType,
-                     actions: Union[List[DispatchKey], Set[DispatchKey]],
-                     expire_minutes: Optional[int] = 60,
-                     any_elements: Optional[list] = None,
-                     any_attributes: Optional[dict] = None) -> ClSubscription:
-        """ creates a subscription object and registers it in
+                     actions: list[DispatchKey] | set[DispatchKey],
+                     expire_minutes: int | None = 60,
+                     any_elements: list | None = None,
+                     any_attributes: dict | None = None) -> ClSubscription:
+        """Creates a subscription object and registers it in
         :param dpws_hosted: proxy for the hosted service that provides the events we want to subscribe to
                            This is the target for all subscribe/unsubscribe ... messages
         :param filter_type: the filter that is sent to device
@@ -350,7 +323,7 @@ class SdcClient:
         :param expire_minutes: defaults to 1 hour
         :param any_elements: optional list of etree.Element objects
         :param any_attributes: optional dictionary of name:str - value:str pairs
-        :return: a subscription object that has callback already registered
+        :return: a subscription object that has callback already registered.
         """
         subscription = self.mk_subscription(dpws_hosted, filter_type, actions)
         properties.bind(subscription, notification_data=self._on_notification)
@@ -358,7 +331,7 @@ class SdcClient:
         return subscription
 
     def client(self, port_type_name):
-        """ returns the client for the given port type name.
+        """Returns the client for the given port type name.
         WDP and SDC use different port type names, e.g. WPF="Get", SDC="GetService".
         If the port type is not found directly, it tries also with or without "Service" in name.
         :param port_type_name: string, e.g "Get", or "GetService", ...
@@ -410,13 +383,12 @@ class SdcClient:
     def subscription_mgr(self):
         return self._subscription_mgr
 
-    def start_all(self, not_subscribed_actions: Optional[list[str]] = None,
-                  subscriptions_check_interval: Optional[float] = None,
+    def start_all(self, not_subscribed_actions: list[str] | None = None,
+                  subscriptions_check_interval: float | None = None,
                   subscribe_periodic_reports: bool = False,
-                  shared_http_server: Optional[Any] = None,
+                  shared_http_server: Any | None = None,
                   check_get_service: bool = True) -> None:
-        """
-        :param not_subscribed_actions: a list of pmtypes.Actions elements or None. if None, everything is subscribed.
+        """:param not_subscribed_actions: a list of pmtypes.Actions elements or None. if None, everything is subscribed.
         :param subscriptions_check_interval: an interval in seconds or None
         :param subscribe_periodic_reports:
         :param shared_http_server: if provided, use this http server, else client creates its own.
@@ -457,9 +429,9 @@ class SdcClient:
         # flag 'self.all_subscribed' tells mdib that mdib state versions shall not have any gaps
         # => log warnings for missing versions
         self.all_subscribed = True
-        not_subscribed_actions_set = set([])
+        not_subscribed_actions_set = set()
         if not_subscribed_actions:
-            not_subscribed_episodic_actions = [a for a in not_subscribed_actions if not 'Periodic' in a]
+            not_subscribed_episodic_actions = [a for a in not_subscribed_actions if "Periodic" not in a]
             if not_subscribed_episodic_actions:
                 self.all_subscribed = False
                 not_subscribed_actions_set = set(not_subscribed_actions)
@@ -481,13 +453,12 @@ class SdcClient:
                     if client is not None:
                         available_actions.extend(client.get_available_subscriptions())
             if len(available_actions) > 0:
-                # subscribe_actions = set(available_actions) - not_subscribed_actions_set
                 subscribe_actions = {a for a in available_actions if a.action not in not_subscribed_actions_set}
                 if not subscribe_periodic_reports:
                     subscribe_actions = {a for a in subscribe_actions if a.action not in periodic_actions}
                 if len(subscribe_actions) > 0:
                     filter_type = eventing_types.FilterType()
-                    filter_type.text = ' '.join((x.action for x in subscribe_actions))
+                    filter_type.text = ' '.join(x.action for x in subscribe_actions)
                     filter_type.Dialect = DeviceEventingFilterDialectURI.ACTION
                     try:
                         self.do_subscribe(dpws_hosted, filter_type, subscribe_actions)
@@ -577,10 +548,7 @@ class SdcClient:
     def _mk_soap_client(self, scheme, netloc, logger, ssl_context, sdc_definitions, msg_reader,
                         supported_encodings=None,
                         request_encodings=None, chunked_requests=False):
-        if scheme == 'https':
-            _ssl_context = ssl_context
-        else:
-            _ssl_context = None
+        _ssl_context = ssl_context if scheme == "https" else None
         cls = self._components.soap_client_class
         return cls(netloc, logger, ssl_context=_ssl_context,
                    sdc_definitions=sdc_definitions,
@@ -612,7 +580,7 @@ class SdcClient:
         for cls in self._components.service_handlers:
             if cls.port_type_name == port_type:
                 return cls(self, soap_client, hosted, port_type)
-        return
+        return None
 
     def _start_event_sink(self, shared_http_server):
         if shared_http_server is None:
@@ -623,7 +591,7 @@ class SdcClient:
                 str(self._network_adapter.ip),
                 ssl_context,
                 logger=logger,
-                supported_encodings=self._compression_methods
+                supported_encodings=self._compression_methods,
             )
             self._http_server.start()
             self._http_server.started_evt.wait(timeout=5)
@@ -646,15 +614,13 @@ class SdcClient:
         self.subscription_end_data = SubscriptionEndData(subscription, request_data)
         return EmptyResponse()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'SdcClient to {self.host_description.this_device} {self.host_description.this_model} on {self._device_location}'
 
     @classmethod
     def from_wsd_service(cls, wsd_service, ssl_context, validate=True, log_prefix='',
                          default_components=None, specific_components=None):
-        """
-
-        :param wsd_service: a wsdiscovery.Service instance
+        """:param wsd_service: a wsdiscovery.Service instance
         :param ssl_context: a ssl context or None
         :param validate: bool
         :param log_prefix: a string
