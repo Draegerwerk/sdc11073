747eef25e3d7d300b864871a481a20c0a17c00cc
Budnick, Leon <leon.budnick@draeger.com>
2023-08-21 11:31:50 +0200

    fix implicit export of modules in __init__

--- src/sdc11073/consumer/__init__.py
+++ src/sdc11073/consumer/__init__.py
@@ -1,1 +1,21 @@
-from .consumerimpl import SdcConsumer
+from sdc11073.consumer.consumerimpl import SdcConsumer
+from sdc11073.consumer.serviceclients.containmenttreeservice import CTreeServiceClient
+from sdc11073.consumer.serviceclients.contextservice import ContextServiceClient
+from sdc11073.consumer.serviceclients.descriptioneventservice import DescriptionEventClient
+from sdc11073.consumer.serviceclients.getservice import GetServiceClient
+from sdc11073.consumer.serviceclients.localizationservice import LocalizationServiceClient
+from sdc11073.consumer.serviceclients.setservice import SetServiceClient
+from sdc11073.consumer.serviceclients.stateeventservice import StateEventClient
+from sdc11073.consumer.serviceclients.waveformservice import WaveformClient
+
+__all__ = [
+    'SdcConsumer',
+    'CTreeServiceClient',
+    'ContextServiceClient',
+    'DescriptionEventClient',
+    'GetServiceClient',
+    'LocalizationServiceClient',
+    'SetServiceClient',
+    'StateEventClient',
+    'WaveformClient',
+]
