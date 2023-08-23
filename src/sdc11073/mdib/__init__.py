b3d1288fa71e68fe8a8c72364688df725f3915d6
Budnick, Leon <leon.budnick@draeger.com>
2023-08-23 14:22:26 +0200

    remove renaming in __init__

--- src/sdc11073/mdib/__init__.py
+++ src/sdc11073/mdib/__init__.py
@@ -1,175 +1,159 @@
 from sdc11073.mdib.consumermdib import ConsumerMdib
-from sdc11073.mdib.descriptorcontainers import AbstractAlertDescriptorContainer as AbstractAlertDescriptor
 from sdc11073.mdib.descriptorcontainers import (
-    AbstractComplexDeviceComponentDescriptorContainer as AbstractComplexDeviceComponentDescriptor,
+    AbstractAlertDescriptorContainer,
+    AbstractComplexDeviceComponentDescriptorContainer,
+    AbstractContextDescriptorContainer,
+    AbstractDescriptorContainer,
+    AbstractDeviceComponentDescriptorContainer,
+    AbstractMetricDescriptorContainer,
+    AbstractOperationDescriptorContainer,
+    AbstractSetStateOperationDescriptorContainer,
+    ActivateOperationDescriptorContainer,
+    AlertConditionDescriptorContainer,
+    AlertSignalDescriptorContainer,
+    AlertSystemDescriptorContainer,
+    BatteryDescriptorContainer,
+    ChannelDescriptorContainer,
+    ClockDescriptorContainer,
+    DistributionSampleArrayMetricDescriptorContainer,
+    EnsembleContextDescriptorContainer,
+    EnumStringMetricDescriptorContainer,
+    LimitAlertConditionDescriptorContainer,
+    LocationContextDescriptorContainer,
+    MdsDescriptorContainer,
+    MeansContextDescriptorContainer,
+    NumericMetricDescriptorContainer,
+    OperatorContextDescriptorContainer,
+    PatientContextDescriptorContainer,
+    RealTimeSampleArrayMetricDescriptorContainer,
+    ScoDescriptorContainer,
+    SetAlertStateOperationDescriptorContainer,
+    SetComponentStateOperationDescriptorContainer,
+    SetContextStateOperationDescriptorContainer,
+    SetMetricStateOperationDescriptorContainer,
+    SetStringOperationDescriptorContainer,
+    SetValueOperationDescriptorContainer,
+    StringMetricDescriptorContainer,
+    SystemContextDescriptorContainer,
+    VmdDescriptorContainer,
+    WorkflowContextDescriptorContainer,
 )
-from sdc11073.mdib.descriptorcontainers import AbstractContextDescriptorContainer as AbstractContextDescriptor
-from sdc11073.mdib.descriptorcontainers import AbstractDescriptorContainer as AbstractDescriptor
-from sdc11073.mdib.descriptorcontainers import AbstractDeviceComponentDescriptorContainer as AbstractDeviceDescriptor
-from sdc11073.mdib.descriptorcontainers import AbstractMetricDescriptorContainer as AbstractMetricDescriptor
-from sdc11073.mdib.descriptorcontainers import AbstractOperationDescriptorContainer as AbstractOperationDescriptor
-from sdc11073.mdib.descriptorcontainers import (
-    AbstractSetStateOperationDescriptorContainer as AbstractSetStateOperationDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import ActivateOperationDescriptorContainer as ActivateOperationDescriptor
-from sdc11073.mdib.descriptorcontainers import AlertConditionDescriptorContainer as AlertConditionDescriptor
-from sdc11073.mdib.descriptorcontainers import AlertSignalDescriptorContainer as AlertSignalDescriptor
-from sdc11073.mdib.descriptorcontainers import AlertSystemDescriptorContainer as AlertSystemDescriptor
-from sdc11073.mdib.descriptorcontainers import BatteryDescriptorContainer as BatteryDescriptor
-from sdc11073.mdib.descriptorcontainers import ChannelDescriptorContainer as ChannelDescriptor
-from sdc11073.mdib.descriptorcontainers import ClockDescriptorContainer as ClockDescriptor
-from sdc11073.mdib.descriptorcontainers import (
-    DistributionSampleArrayMetricDescriptorContainer as DistributionSampleArrayMetricDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import EnsembleContextDescriptorContainer as EnsembleContextDescriptor
-from sdc11073.mdib.descriptorcontainers import EnumStringMetricDescriptorContainer as EnumStringMetricDescriptor
-from sdc11073.mdib.descriptorcontainers import LimitAlertConditionDescriptorContainer as LimitAlertConditionDescriptor
-from sdc11073.mdib.descriptorcontainers import LocationContextDescriptorContainer as LocationContextDescriptor
-from sdc11073.mdib.descriptorcontainers import MdsDescriptorContainer as MdsDescriptor
-from sdc11073.mdib.descriptorcontainers import MeansContextDescriptorContainer as MeansContextDescriptor
-from sdc11073.mdib.descriptorcontainers import NumericMetricDescriptorContainer as NumericMetricDescriptor
-from sdc11073.mdib.descriptorcontainers import OperatorContextDescriptorContainer as OperatorContextDescriptor
-from sdc11073.mdib.descriptorcontainers import PatientContextDescriptorContainer as PatientContextDescriptor
-from sdc11073.mdib.descriptorcontainers import (
-    RealTimeSampleArrayMetricDescriptorContainer as RealTimeSampleArrayMetricDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import ScoDescriptorContainer as ScoDescriptor
-from sdc11073.mdib.descriptorcontainers import (
-    SetAlertStateOperationDescriptorContainer as SetAlertStateOperationDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import (
-    SetComponentStateOperationDescriptorContainer as SetComponentStateOperationDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import (
-    SetContextStateOperationDescriptorContainer as SetContextStateOperationDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import (
-    SetMetricStateOperationDescriptorContainer as SetMetricStateOperationDescriptor,
-)
-from sdc11073.mdib.descriptorcontainers import SetStringOperationDescriptorContainer as SetStringOperationDescriptor
-from sdc11073.mdib.descriptorcontainers import SetValueOperationDescriptorContainer as SetValueOperationDescriptor
-from sdc11073.mdib.descriptorcontainers import StringMetricDescriptorContainer as StringMetricDescriptor
-from sdc11073.mdib.descriptorcontainers import SystemContextDescriptorContainer as SystemContextDescriptor
-from sdc11073.mdib.descriptorcontainers import VmdDescriptorContainer as VmdDescriptor
-from sdc11073.mdib.descriptorcontainers import WorkflowContextDescriptorContainer as WorkflowContextDescriptor
 from sdc11073.mdib.providermdib import ProviderMdib
-from sdc11073.mdib.statecontainers import AbstractAlertStateContainer as AbstractAlertState
 from sdc11073.mdib.statecontainers import (
-    AbstractComplexDeviceComponentStateContainer as AbstractComplexDeviceComponentState,
+    AbstractAlertStateContainer,
+    AbstractComplexDeviceComponentStateContainer,
+    AbstractContextStateContainer,
+    AbstractDeviceComponentStateContainer,
+    AbstractMetricStateContainer,
+    AbstractOperationStateContainer,
+    AbstractStateContainer,
+    ActivateOperationStateContainer,
+    AlertConditionStateContainer,
+    AlertSignalStateContainer,
+    AlertSystemStateContainer,
+    AllowedValuesType,
+    BatteryStateContainer,
+    ChannelStateContainer,
+    ClockStateContainer,
+    DistributionSampleArrayMetricStateContainer,
+    EnsembleContextStateContainer,
+    EnumStringMetricStateContainer,
+    LimitAlertConditionStateContainer,
+    LocationContextStateContainer,
+    MdsStateContainer,
+    MeansContextStateContainer,
+    NumericMetricStateContainer,
+    OperatorContextStateContainer,
+    PatientContextStateContainer,
+    RealTimeSampleArrayMetricStateContainer,
+    ScoStateContainer,
+    SetAlertStateOperationStateContainer,
+    SetComponentStateOperationStateContainer,
+    SetContextStateOperationStateContainer,
+    SetMetricStateOperationStateContainer,
+    SetStringOperationStateContainer,
+    SetValueOperationStateContainer,
+    StringMetricStateContainer,
+    SystemContextStateContainer,
+    VmdStateContainer,
+    WorkflowContextStateContainer,
 )
-from sdc11073.mdib.statecontainers import AbstractContextStateContainer as AbstractContextState
-from sdc11073.mdib.statecontainers import AbstractDeviceComponentStateContainer as AbstractDeviceState
-from sdc11073.mdib.statecontainers import AbstractMetricStateContainer as AbstractMetricState
-from sdc11073.mdib.statecontainers import AbstractOperationStateContainer as AbstractOperationState
-from sdc11073.mdib.statecontainers import AbstractStateContainer as AbstractState
-from sdc11073.mdib.statecontainers import ActivateOperationStateContainer as ActivateOperationState
-from sdc11073.mdib.statecontainers import AlertConditionStateContainer as AlertConditionState
-from sdc11073.mdib.statecontainers import AlertSignalStateContainer as AlertSignalState
-from sdc11073.mdib.statecontainers import AlertSystemStateContainer as AlertSystemState
-from sdc11073.mdib.statecontainers import AllowedValuesType
-from sdc11073.mdib.statecontainers import BatteryStateContainer as BatteryState
-from sdc11073.mdib.statecontainers import ChannelStateContainer as ChannelState
-from sdc11073.mdib.statecontainers import ClockStateContainer as ClockState
-from sdc11073.mdib.statecontainers import (
-    DistributionSampleArrayMetricStateContainer as DistributionSampleArrayMetricState,
-)
-from sdc11073.mdib.statecontainers import EnsembleContextStateContainer as EnsembleContextState
-from sdc11073.mdib.statecontainers import EnumStringMetricStateContainer as EnumStringMetricState
-from sdc11073.mdib.statecontainers import LimitAlertConditionStateContainer as LimitAlertConditionState
-from sdc11073.mdib.statecontainers import LocationContextStateContainer as LocationContextState
-from sdc11073.mdib.statecontainers import MdsStateContainer as MdsState
-from sdc11073.mdib.statecontainers import MeansContextStateContainer as MeansContextState
-from sdc11073.mdib.statecontainers import NumericMetricStateContainer as NumericMetricState
-from sdc11073.mdib.statecontainers import OperatorContextStateContainer as OperatorContextState
-from sdc11073.mdib.statecontainers import PatientContextStateContainer as PatientContextState
-from sdc11073.mdib.statecontainers import RealTimeSampleArrayMetricStateContainer as RealTimeSampleArrayMetricState
-from sdc11073.mdib.statecontainers import ScoStateContainer as ScoState
-from sdc11073.mdib.statecontainers import SetAlertStateOperationStateContainer as SetAlertStateOperationState
-from sdc11073.mdib.statecontainers import SetComponentStateOperationStateContainer as SetComponentStateOperationState
-from sdc11073.mdib.statecontainers import SetContextStateOperationStateContainer as SetContextStateOperationState
-from sdc11073.mdib.statecontainers import SetMetricStateOperationStateContainer as SetMetricStateOperationState
-from sdc11073.mdib.statecontainers import SetStringOperationStateContainer as SetStringOperationState
-from sdc11073.mdib.statecontainers import SetValueOperationStateContainer as SetValueOperationState
-from sdc11073.mdib.statecontainers import StringMetricStateContainer as StringMetricState
-from sdc11073.mdib.statecontainers import SystemContextStateContainer as SystemContextState
-from sdc11073.mdib.statecontainers import VmdStateContainer as VmdState
-from sdc11073.mdib.statecontainers import WorkflowContextStateContainer as WorkflowContextState
 
 __all__ = [
     'ConsumerMdib',
     'ProviderMdib',
-    'AbstractDescriptor',
-    'AbstractDeviceDescriptor',
-    'AbstractComplexDeviceComponentDescriptor',
-    'MdsDescriptor',
-    'VmdDescriptor',
-    'ChannelDescriptor',
-    'ClockDescriptor',
-    'BatteryDescriptor',
-    'ScoDescriptor',
-    'AbstractMetricDescriptor',
-    'NumericMetricDescriptor',
-    'StringMetricDescriptor',
-    'EnumStringMetricDescriptor',
-    'RealTimeSampleArrayMetricDescriptor',
-    'DistributionSampleArrayMetricDescriptor',
-    'AbstractOperationDescriptor',
-    'SetValueOperationDescriptor',
-    'SetStringOperationDescriptor',
-    'AbstractSetStateOperationDescriptor',
-    'SetContextStateOperationDescriptor',
-    'SetMetricStateOperationDescriptor',
-    'SetComponentStateOperationDescriptor',
-    'SetAlertStateOperationDescriptor',
-    'ActivateOperationDescriptor',
-    'AbstractAlertDescriptor',
-    'AlertSystemDescriptor',
-    'AlertConditionDescriptor',
-    'LimitAlertConditionDescriptor',
-    'AlertSignalDescriptor',
-    'SystemContextDescriptor',
-    'AbstractContextDescriptor',
-    'PatientContextDescriptor',
-    'LocationContextDescriptor',
-    'WorkflowContextDescriptor',
-    'OperatorContextDescriptor',
-    'MeansContextDescriptor',
-    'EnsembleContextDescriptor',
-    'AbstractState',
-    'AbstractDeviceState',
-    'AbstractComplexDeviceComponentState',
+    'AbstractDescriptorContainer',
+    'AbstractDeviceComponentDescriptorContainer',
+    'AbstractComplexDeviceComponentDescriptorContainer',
+    'MdsDescriptorContainer',
+    'VmdDescriptorContainer',
+    'ChannelDescriptorContainer',
+    'ClockDescriptorContainer',
+    'BatteryDescriptorContainer',
+    'ScoDescriptorContainer',
+    'AbstractMetricDescriptorContainer',
+    'NumericMetricDescriptorContainer',
+    'StringMetricDescriptorContainer',
+    'EnumStringMetricDescriptorContainer',
+    'RealTimeSampleArrayMetricDescriptorContainer',
+    'DistributionSampleArrayMetricDescriptorContainer',
+    'AbstractOperationDescriptorContainer',
+    'SetValueOperationDescriptorContainer',
+    'SetStringOperationDescriptorContainer',
+    'AbstractSetStateOperationDescriptorContainer',
+    'SetContextStateOperationDescriptorContainer',
+    'SetMetricStateOperationDescriptorContainer',
+    'SetComponentStateOperationDescriptorContainer',
+    'SetAlertStateOperationDescriptorContainer',
+    'ActivateOperationDescriptorContainer',
+    'AbstractAlertDescriptorContainer',
+    'AlertSystemDescriptorContainer',
+    'AlertConditionDescriptorContainer',
+    'LimitAlertConditionDescriptorContainer',
+    'AlertSignalDescriptorContainer',
+    'SystemContextDescriptorContainer',
+    'AbstractContextDescriptorContainer',
+    'PatientContextDescriptorContainer',
+    'LocationContextDescriptorContainer',
+    'WorkflowContextDescriptorContainer',
+    'OperatorContextDescriptorContainer',
+    'MeansContextDescriptorContainer',
+    'EnsembleContextDescriptorContainer',
+    'AbstractStateContainer',
+    'AbstractDeviceComponentStateContainer',
+    'AbstractComplexDeviceComponentStateContainer',
     'AllowedValuesType',
-    'MdsState',
-    'VmdState',
-    'ChannelState',
-    'ClockState',
-    'BatteryState',
-    'ScoState',
-    'AbstractMetricState',
-    'NumericMetricState',
-    'StringMetricState',
-    'EnumStringMetricState',
-    'RealTimeSampleArrayMetricState',
-    'DistributionSampleArrayMetricState',
-    'AbstractOperationState',
-    'SetValueOperationState',
-    'SetStringOperationState',
-    'SetContextStateOperationState',
-    'SetMetricStateOperationState',
-    'SetComponentStateOperationState',
-    'SetAlertStateOperationState',
-    'ActivateOperationState',
-    'AbstractAlertState',
-    'AlertSystemState',
-    'AlertConditionState',
-    'LimitAlertConditionState',
-    'AlertSignalState',
-    'SystemContextState',
-    'AbstractContextState',
-    'PatientContextState',
-    'LocationContextState',
-    'WorkflowContextState',
-    'OperatorContextState',
-    'MeansContextState',
-    'EnsembleContextState',
+    'MdsStateContainer',
+    'VmdStateContainer',
+    'ChannelStateContainer',
+    'ClockStateContainer',
+    'BatteryStateContainer',
+    'ScoStateContainer',
+    'AbstractMetricStateContainer',
+    'NumericMetricStateContainer',
+    'StringMetricStateContainer',
+    'EnumStringMetricStateContainer',
+    'RealTimeSampleArrayMetricStateContainer',
+    'DistributionSampleArrayMetricStateContainer',
+    'AbstractOperationStateContainer',
+    'SetValueOperationStateContainer',
+    'SetStringOperationStateContainer',
+    'SetContextStateOperationStateContainer',
+    'SetMetricStateOperationStateContainer',
+    'SetComponentStateOperationStateContainer',
+    'SetAlertStateOperationStateContainer',
+    'ActivateOperationStateContainer',
+    'AbstractAlertStateContainer',
+    'AlertSystemStateContainer',
+    'AlertConditionStateContainer',
+    'LimitAlertConditionStateContainer',
+    'AlertSignalStateContainer',
+    'SystemContextStateContainer',
+    'AbstractContextStateContainer',
+    'PatientContextStateContainer',
+    'LocationContextStateContainer',
+    'WorkflowContextStateContainer',
+    'OperatorContextStateContainer',
+    'MeansContextStateContainer',
+    'EnsembleContextStateContainer',
 ]
