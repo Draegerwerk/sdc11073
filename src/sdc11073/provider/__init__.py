235b750bb90df156c51ed1e707ff444f1ab2d919
Leon <leon.budnick@draeger.com>
2023-08-24 12:48:43 +0200

    fix implicit export of modules in __init__ (#240)
    <!--- Provide a general summary of your changes in the title above -->
    <!--- Link the corresponding issues after you created the pull request
    -->
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] I have updated the [changelog](../CHANGELOG.md) accordingly.
    - [ ] I have added tests to cover my changes.

--- src/sdc11073/provider/__init__.py
+++ src/sdc11073/provider/__init__.py
@@ -1,5 +1,21 @@
+from sdc11073.provider.porttypes.containmenttreeserviceimpl import ContainmentTreeService
+from sdc11073.provider.porttypes.contextserviceimpl import ContextService
+from sdc11073.provider.porttypes.descriptioneventserviceimpl import DescriptionEventService
+from sdc11073.provider.porttypes.getserviceimpl import GetService
+from sdc11073.provider.porttypes.localizationservice import LocalizationService
+from sdc11073.provider.porttypes.setserviceimpl import SetService
+from sdc11073.provider.porttypes.stateeventserviceimpl import StateEventService
+from sdc11073.provider.porttypes.waveformserviceimpl import WaveformService
 from sdc11073.provider.providerimpl import SdcProvider
 
 __all__ = [
     'SdcProvider',
+    'ContainmentTreeService',
+    'ContextService',
+    'DescriptionEventService',
+    'GetService',
+    'LocalizationService',
+    'SetService',
+    'StateEventService',
+    'WaveformService',
 ]
