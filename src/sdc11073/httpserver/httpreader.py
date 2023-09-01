08207ad1e815c1b0bdbd5c67446fe7323453091b
Budnick, Leon <leon.budnick@draeger.com>
2023-09-01 12:01:31 +0200

    fix UP012

--- src/sdc11073/httpserver/httpreader.py
+++ src/sdc11073/httpserver/httpreader.py
@@ -27,7 +27,7 @@ def mk_chunks(body, chunk_size=512):
     tail = body
     while True:
         head, tail = tail[:chunk_size], tail[chunk_size:]
-        data.write(f'{len(head):x}\r\n'.encode('utf-8'))
+        data.write(f'{len(head):x}\r\n'.encode())
         data.write(head)
         data.write(b'\r\n')
         if not head:
