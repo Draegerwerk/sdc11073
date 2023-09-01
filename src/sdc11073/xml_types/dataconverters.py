f3fa6fe61b211a47c649643d9a5a94649a2acf1c
Budnick, Leon <leon.budnick@draeger.com>
2023-09-01 12:04:25 +0200

    fix RET505

--- src/sdc11073/xml_types/dataconverters.py
+++ src/sdc11073/xml_types/dataconverters.py
@@ -67,10 +67,7 @@ class EnumConverter(NullConverter):
         return value
 
     def to_xml(self, py_value):
-        if hasattr(py_value, 'value'):
-            return py_value.value
-        else:
-            return py_value
+        return py_value.value if hasattr(py_value, 'value') else py_value
 
     def check_valid(self, py_value) -> bool:
         if STRICT_VALUE_CHECK and py_value is not None:
