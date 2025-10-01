2c872922e61fb6e1213459611f0a6d88327db14f
Budnick, Leon <leon.budnick@draeger.com>
2025-10-01 10:13:00 +0200

    revert provider http server address

--- src/sdc11073/provider/providerimpl.py
+++ src/sdc11073/provider/providerimpl.py
@@ -462,9 +462,8 @@ class SdcProvider:
             self._is_internal_http_server = True
             logger = loghelper.get_logger_adapter('sdc.device.httpsrv', self._log_prefix)
 
-            bind_ip = '0.0.0.0' if self._alternative_hostname else self._wsdiscovery.get_active_addresses()[0]  # noqa: S104
             self._http_server = HttpServerThreadBase(
-                my_ipaddress=bind_ip,
+                my_ipaddress='0.0.0.0',  # noqa: S104,
                 ssl_context=self._ssl_context_container.server_context if self._ssl_context_container else None,
                 supported_encodings=self._compression_methods,
                 logger=logger,
