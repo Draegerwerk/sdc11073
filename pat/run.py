a2f8d3b378968a762268492e39020ec5348b4167
Budnick, Leon <leon.budnick@draeger.com>
2025-10-22 14:49:26 +0200

    fix tls in run

--- pat/run.py
+++ pat/run.py
@@ -76,11 +76,17 @@ if __name__ == '__main__':
     parser.add_argument('--network-delay', type=float, help='Network delay to use in seconds.', default=0.1)
 
     args = parser.parse_args()
+
     passed = run(
         mdib_path=args.mdib_path,
         adapter=args.adapter,
         epr=args.epr,
-        ssl_context_container=common.get_ssl_context(args.certificate_folder, args.ssl_password) if args.tls else None,
+        ssl_context_container=common.get_ssl_context(
+            pathlib.Path(__file__).parent.joinpath('certs').resolve(),
+            'dummypass',
+        )
+        if args.tls
+        else None,
         network_delay=args.network_delay,
     )
     sys.exit(0 if passed else 1)
