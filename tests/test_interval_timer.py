5c97e0afad249c739bfb43ffe68ff4222c5e7d29
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2025-11-11 12:08:01 +0100

    Fix test for IntervallTimer

--- tests/test_interval_timer.py
+++ tests/test_interval_timer.py
@@ -1,3 +1,5 @@
+"""Tests for IntervalTimer."""
+
 import time
 from unittest import TestCase
 
@@ -22,13 +24,16 @@ class TestIntervalTimer(TestCase):
         """Verify that delay calculation is correct."""
         period = 0.1
         loops = 50
-        too_long = 0.1
+        too_long = 0.2
+        start = time.perf_counter()
         it = IntervalTimer(period_in_seconds=period)
         behind = it.wait_next_interval_begin()
         self.assertEqual(behind, 0)
+
         for _ in range(loops):
             time.sleep(period + too_long)  # sleep a little longer than period, timer cannot catch up.
             behind = it.wait_next_interval_begin()
             print(behind)
+        overall_calc_time_too_long = (time.perf_counter() - start) - (loops * period)
         self.assertGreater(behind, loops * too_long)
-        self.assertLess(behind, loops * too_long + 0.4)
+        self.assertLess(behind, overall_calc_time_too_long)
