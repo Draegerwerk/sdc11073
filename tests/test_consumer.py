f49fe9f6f7984897b2cb6c1856a1ff5fabc90593
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2026-01-27 21:43:57 +0100

    Format file

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,20 +1,20 @@
 """Tests for SDC Consumer functionality."""
 
-import threading
 import unittest
 from unittest import mock
+
 from sdc11073.consumer import SdcConsumer
 from sdc11073.definitions_sdc import SdcV1Definitions
 from sdc11073.httpserver.httpserverimpl import HttpServerThreadBase
 
-class TestConsumerWithFailure(unittest.TestCase):
 
+class TestConsumerWithFailure(unittest.TestCase):
     def test_fail_on_startup(self):
         """Test that starting services fails with RuntimeError if HTTP server does not start in time."""
-        with mock.patch.object(HttpServerThreadBase, "start"):
+        with mock.patch.object(HttpServerThreadBase, 'start'):
             sdc_consumer = SdcConsumer('http', SdcV1Definitions, None)
             sdc_consumer._network_adapter = mock.Mock()
-            sdc_consumer._network_adapter.ip = "123.456.789.000"
+            sdc_consumer._network_adapter.ip = '123.456.789.000'
 
             with self.assertRaises(RuntimeError) as context:
                 sdc_consumer._start_event_sink(shared_http_server=None, http_server_timeout=0)
