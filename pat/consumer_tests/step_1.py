1bcb1b1ec0c7133b91539d13ec05ec8aab36607a
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2026-03-13 16:15:30 +0100

    Add parameter to allow consumer to wait for provier to be started

--- pat/consumer_tests/step_1.py
+++ pat/consumer_tests/step_1.py
@@ -3,6 +3,7 @@
 import functools
 import logging
 import threading
+import time
 from collections.abc import Sequence
 
 from sdc11073 import definitions_sdc, wsdiscovery
@@ -49,18 +50,26 @@ def on_resolve_match(step: str, event: threading.Event, expected_epr: str, servi
         event.set()
 
 
-def test_1b(discovery: wsdiscovery.WSDiscovery, epr: str) -> bool:
+def test_1b(discovery: wsdiscovery.WSDiscovery, epr: str, timeout: float) -> bool:
     """The Reference Provider answers to Probe and Resolve messages in ad-hoc mode."""
     step = f'{__STEP__}b'
-    timeout = 10.0
-
+    # wait at least 10s for probe matches to be received, but not more than total timeout
+    # it is also ensured that at most every 10s a probe is sent
+    timeout_probe_matches = min(max(timeout / 5, 10), timeout)
     # if the epr is already known you can directly use resolve, but test specification requires probe to be tested
     probe_matches_event = threading.Event()
     observer = functools.partial(on_probe_matches, step, probe_matches_event, epr)
     try:
         discovery.set_on_probe_matches_callback(observer)
-        discovery._send_probe(types=definitions_sdc.SdcV1Definitions.MedicalDeviceTypesFilter)  # noqa: SLF001
-        result = probe_matches_event.wait(timeout)
+        start = time.perf_counter()
+        iteration = 0
+        while True:
+            iteration += 1
+            logger.info('Send probe messages - iteration: %s', iteration, extra={'step': step})
+            discovery._send_probe(types=definitions_sdc.SdcV1Definitions.MedicalDeviceTypesFilter)  # noqa: SLF001
+            result = probe_matches_event.wait(timeout_probe_matches)
+            if result or (time.perf_counter() - start) > timeout:
+                break
     finally:
         discovery.set_on_probe_matches_callback(None)
     if result:
