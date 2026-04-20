18573a68d39c26d842e0c3fd0a6fb8fa63b8f492
Budnick, Leon <leon.budnick@draeger.com>
2026-04-20 09:01:22 +0200

    fix tests

--- tests/test_subscription_manager_base.py
+++ tests/test_subscription_manager_base.py
@@ -261,7 +261,7 @@ def test_end_to_url_is_set_when_end_to_provided(soap_client_pool: mock.MagicMock
     sub.send_notification_end_message()
     soap_client_pool.get_soap_client.assert_called_once_with('localhost:9000', [], sub)
     soap_client_pool.get_soap_client.return_value.post_message_to.assert_called_once_with(
-        '/notify', mock.ANY, msg='send_notification_end_message'
+        '/end', mock.ANY, msg='send_notification_end_message'
     )
 
 
@@ -289,5 +289,5 @@ def test_end_to_url_is_none_when_end_to_address_is_none(soap_client_pool: mock.M
     sub.send_notification_end_message()
     soap_client_pool.get_soap_client.assert_called_once_with('localhost:8000', [], sub)
     soap_client_pool.get_soap_client.return_value.post_message_to.assert_called_once_with(
-        '/end', mock.ANY, msg='send_notification_end_message'
+        '/notify', mock.ANY, msg='send_notification_end_message'
     )
