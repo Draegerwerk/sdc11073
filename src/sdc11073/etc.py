093999829c96e1fa54052602e52718225bf55bdd
Budnick, Leon <leon.budnick@draeger.com>
2026-07-13 15:23:19 +0200

    fix

--- src/sdc11073/etc.py
+++ src/sdc11073/etc.py
@@ -1,13 +1,12 @@
 """Small helper functions shared across the sdc11073 library."""
 
 from collections.abc import Callable, Iterable, Sequence
-from typing import TypeVar
+from typing import Any, TypeVar
 
-P = TypeVar('P')
 R = TypeVar('R')
 
 
-def apply_map(function: Callable[P, R], *iterable: P) -> Sequence[R]:
+def apply_map(function: Callable[..., R], *iterable: Iterable[Any]) -> Sequence[R]:
     """Call function for all elements of iterable(s).
 
     apply_map uses builtin map internally.
